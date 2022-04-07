a75133b159f340168a85ecb967b9f14e8319e72b
* <abdulazeeztijanisani@gmail.com>
2022-04-07 23:20:14 +0100

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,11 +7,13 @@
  **/
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+void *p;
 
-	p = malloc(b);
+p = malloc(b);
 
-	if (p == NULL)
+if (p == NULL)
 		exit(98);
-	return (p);
 }
+return (p);
+}
+
